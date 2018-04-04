813ff271e114a978775378eb2b897b532c8adab6
Anirudh Emmadi <kanna6501@gmail.com>
2018-04-04 10:33:37 -0500

    Optimized Form Backend

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,8 +15,8 @@ $message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'contact@anirudhemmadi.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Anirudh Emmadi Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$email_subject = "Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form @ http://www.anirudhemmadi.com .\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@anirudhemmadi.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
